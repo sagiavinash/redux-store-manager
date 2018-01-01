a4f58be6e7604ae069b6b1c935075dfb253430f9
Avinash Varma <s.avinash.v@uber.com>
2018-01-01 18:05:48 +0530

    add basic reducer functionality - query, isLoading, data, error

---
+++ example/index.js
@@ -0,0 +1,27 @@
+// import reduxReducer, {createAsyncTaskActions} = 'redux-reducers';
+
+const reduxReducer = require('../dist/redux-reducers').default;
+const createAsyncTaskActions = require('../dist/redux-reducers').createAsyncTaskActions;
+
+const getInfoTaskId = 'GET_INFO';
+
+const getInfoActions = createAsyncTaskActions(getInfoTaskId);
+const infoReducer = reduxReducer(getInfoTaskId);
+
+const startAction = getInfoActions.start({id: 1});
+const successAction = getInfoActions.end({id: 1, title: 'hello'});
+const failureAction = getInfoActions.end(new Error('Internal Server Error'));
+
+const initialState = infoReducer(undefined, {});
+const startAfterState = infoReducer(initialState, startAction);
+const startAfterSuccess = infoReducer(initialState, successAction);
+const startAfterFailure = infoReducer(initialState, failureAction);
+
+console.log('startAction', startAction);
+console.log('successAction', successAction);
+console.log('failureAction', failureAction);
+
+console.log('initialState', initialState);
+console.log('startAfterState', startAfterState);
+console.log('startAfterSuccess', startAfterSuccess);
+console.log('startAfterFailure', startAfterFailure);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// vendor modules
+var webpack = require('webpack');
+var path = require('path');
+
+
+var config = {
+  entry: path.resolve(__dirname, './lib/'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'redux-reducers.js',
+    library: 'redux-reducers',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015'],
+      },
+    }],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+  ],
+};
+
+module.exports = config;
